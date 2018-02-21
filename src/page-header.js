33dfb4b21baa2ebeaa944f52c1882f2494630c95
unfriend <t-moore@mail.ru>
2018-02-21 15:38:49 +0300

    * search results

--- src/page-header.js
+++ src/page-header.js
@@ -1,6 +1,22 @@
 import React from "react";
 
 export default class PageHeader extends React.Component {
+    showSearchResults() {
+        const resultsBlock = document.getElementById("search-results");
+        if (!resultsBlock) {
+            return;
+        }
+        resultsBlock.style.display = "block";
+    }
+
+    hideSearchResults() {
+        const resultsBlock = document.getElementById("search-results");
+        if (!resultsBlock) {
+            return;
+        }
+        resultsBlock.style.display = "none";
+    }
+
     render() {
         const searchResult = (this.props.searchResult || []).map(function(elem) {
             return (
@@ -21,15 +37,28 @@ export default class PageHeader extends React.Component {
                         </div>
 
                         <div className="page-header__search">
-                            <input type="search" value={ this.props.searchValue } onChange={ (e) => this.props.onSearchChange(e) } placeholder="Событие, дата или участник" />
+                            <input type="search" id="search-input" value={ this.props.searchValue } onChange={ (e) => this.props.onSearchChange(e) } placeholder="Событие, дата или участник" />
 
-                            <div className="page-header__s-results event-popup">
-                                { searchResult }
+                            <div className="page-header__s-results event-popup" id="search-results" style={{ "visibility": (this.props.searchValue.trim() !== "" ? "" : "hidden") }}>
+                                {
+                                    (this.props.searchValue.trim() !== "" && searchResult.length > 0) &&
+                                    searchResult
+                                }
+                                {
+                                    (this.props.searchValue.trim() !== "" && searchResult.length <= 0) &&
+                                    <span>Нет подходящих событий</span>
+                                }
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         );
+    }
+
+    componentDidMount() {
+        const searchInput = document.getElementById("search-input");
+        searchInput.addEventListener("focus", this.showSearchResults);
+        searchInput.addEventListener("blur", this.hideSearchResults);
     }
 }
